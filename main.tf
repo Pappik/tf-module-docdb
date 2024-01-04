1a010de7cf33a500de21e4c28add2b20133b177d
pappik <pappik@gmail.com>
2024-01-04 13:36:33 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -28,3 +28,15 @@ resource "aws_security_group" "docdb" {
 
   tags = merge(local.common_tags, { Name = "${var.env}-docdb_security_group"} )
 }
+
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "${var.env}-docdb_cluster"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  skip_final_snapshot     = true
+  db_subnet_group_name = aws_docdb_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.docdb.id]
+
+  tags = merge(local.common_tags, { Name = "${var.env}-docdb_security_group"} )
+}
