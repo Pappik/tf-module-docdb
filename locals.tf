795c605451198eb59ac4d48d18d02b1950c32df7
pappik <pappik@gmail.com>
2024-01-04 15:43:06 -0500

    vpc terraform

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
-locals {
-  common_tags = {
-    env = var.env
-    project = "roboshop"
-    business_unit = "ecommerce"
-    owner = "ecommerce-roboshop"
-  }
-}
+#locals {
+#  common_tags = {
+#    env = var.env
+#    project = "roboshop"
+#    business_unit = "ecommerce"
+#    owner = "ecommerce-roboshop"
+#  }
+#}
